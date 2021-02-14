73ac4bffca0ceebb812216448ca7fd1b83efd6ed
dhruvik7 <dhruvik.parikh@hotmail.com>
2021-02-14 00:01:09 -0500

    initial commit

--- main.ts
+++ main.ts
@@ -1,60 +1,103 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { TFile, Plugin } from 'obsidian';
 
-interface MyPluginSettings {
-	mySetting: string;
+interface DayStats {
+	date: Date;
+	wordCount: number;
+	initialNoteWordCount: Record<string, number>;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface DailyStatsSettings {
+	stats: DayStats[];
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const DEFAULT_SETTINGS: DailyStatsSettings = {
+	stats: []
+}
+
+export default class DailyStats extends Plugin {
+	settings: DailyStatsSettings;
+	currentFile: TFile;
+	statusBar: StatusBar;
+	currentWordCount: number;
 
 	async onload() {
-		console.log('loading plugin');
-
 		await this.loadSettings();
 
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
-		});
+		let statusBarEl = this.addStatusBarItem();
+		this.statusBar = new StatusBar(statusBarEl);
 
-		this.addStatusBarItem().setText('Status Bar Text');
+		let files: TFile[] = this.app.vault.getMarkdownFiles();
+		this.currentWordCount = 0;
+		for (const file of files) {
+			const toAdd = await this.getTodaysWords(file);
+			this.currentWordCount += toAdd;
+		};
 
-		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
-			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-					return true;
-				}
-				return false;
-			}
-		});
-
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			console.log('codemirror', cm);
-		});
-
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerInterval(
+			window.setInterval(async () => {
+				let files: TFile[] = this.app.vault.getMarkdownFiles();
+				var todaysWords = 0;
+				for (const file of files) {
+					const toAdd = await this.getTodaysWords(file);
+					todaysWords += toAdd;
+				};
+				this.currentWordCount = todaysWords;
+				this.statusBar.displayText(this.currentWordCount + " words today ");
+				this.settings.stats.find((dayStat) => isSameDay(new Date(dayStat.date), new Date())).wordCount = this.currentWordCount;
+				this.saveSettings();
+			}, 500)
+		);
 	}
 
 	onunload() {
-		console.log('unloading plugin');
+		var currDayStat = this.settings.stats.find((dayStat) => isSameDay(new Date(dayStat.date), new Date()));
+		if (currDayStat) {
+			currDayStat.wordCount = this.currentWordCount;
+			this.saveSettings();
+		}
+	}
+
+	//Credit: better-word-count by Luke Leppan (https://github.com/lukeleppan/better-word-count)
+	getWordCount(text: string) {
+		let words: number = 0;
+
+		const matches = text.match(
+			/[a-zA-Z0-9_\u0392-\u03c9\u00c0-\u00ff\u0600-\u06ff]+|[\u4e00-\u9fff\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af]+/gm
+		);
+
+		if (matches) {
+			for (let i = 0; i < matches.length; i++) {
+				if (matches[i].charCodeAt(0) > 19968) {
+					words += matches[i].length;
+				} else {
+					words += 1;
+				}
+			}
+		}
+
+		return words;
+	}
+
+	async getTodaysWords(file: TFile) {
+		const contents = await this.app.vault.cachedRead(file);
+		const curr = this.getWordCount(contents);
+		var dayStat = this.settings.stats.find((dayStat) => isSameDay(new Date(dayStat.date), new Date()));
+		var prev = curr;
+		if (dayStat) {
+			if (file.name in dayStat.initialNoteWordCount) {
+				prev = dayStat.initialNoteWordCount[file.name];
+			} else {
+				dayStat.initialNoteWordCount[file.name] = curr;
+				await this.saveSettings();
+			}
+		} else {
+			this.currentWordCount = 0;
+			var newRecord: Record<string, number> = {};
+			newRecord[file.name] = curr;
+			this.settings.stats.push({ date: new Date(), wordCount: 0, initialNoteWordCount: newRecord });
+			await this.saveSettings();
+		}
+		return Math.max(0, curr - prev);
 	}
 
 	async loadSettings() {
@@ -66,47 +109,19 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+class StatusBar {
+	private statusBarEl: HTMLElement;
+
+	constructor(statusBarEl: HTMLElement) {
+		this.statusBarEl = statusBarEl;
 	}
 
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
+	displayText(text: string) {
+		this.statusBarEl.setText(text);
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		let {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue('')
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
+const isSameDay = (first: Date, second: Date) =>
+	first.getFullYear() === second.getFullYear() &&
+	first.getMonth() === second.getMonth() &&
+	first.getDate() === second.getDate();
