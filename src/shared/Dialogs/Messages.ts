3e965a3ac31fa6f184047cc465bfe146a573f583
zsviczian <viczian.zsolt@gmail.com>
2026-03-12 22:49:04 +0100

    0.18.0-83

--- src/shared/Dialogs/Messages.ts
+++ src/shared/Dialogs/Messages.ts
@@ -24,6 +24,8 @@ I build this plugin in my free time, as a labor of love. Curious about the philo
 ## New
 - LaTeX editor is now floating [#2684](https://github.com/zsviczian/obsidian-excalidraw-plugin/pull/2684) 🙏[@TravisLEBLANC1](https://github.com/TravisLEBLANC1)
 - Inline link suggester now supports triggers \`![[drawing#^frame=\` and \`![[drawing#^clippedframe=\`, thus it is easy to reference frames like slides or parts of images in your different drawings.
+- Support for mermaid Entity Relationship Diagrams, new ERD/cardinality arrowheads [#10940](https://github.com/excalidraw/excalidraw/pull/10940)
+- New Context Menu options to disable arrow binding and midpoint snapping. [#10906](https://github.com/excalidraw/excalidraw/pull/10906)
 `,
 "2.20.6":`
 <div class="excalidraw-videoWrapper">
