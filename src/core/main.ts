zsviczian/obsidian-excalidraw-plugin

commit d3232f3071fd5e3af72810b7440597719857703c

Mindless cosmetics based on Obsidian codescanner results

src/shared/ImageCache.ts
@@ -4,7 +4,8 @@ import { PDFPageViewProps, Size } from "src/types/embeddedFileLoaderTypes";
 import { convertSVGStringToElement } from "../utils/utils";
 import { FILENAMEPARTS, PreviewImageType } from "../types/utilTypes";
 import { hasExcalidrawEmbeddedImagesTreeChanged } from "../utils/fileUtils";
-const DB_NAME = `Excalidraw ${(app as unknown as { appId: string }).appId}`;
+import { EXCALIDRAW_PLUGIN } from "src/constants/constants";
+const DB_NAME = `Excalidraw ${EXCALIDRAW_PLUGIN.app.appId}`;
 const CACHE_STORE = "imageCache";
 const BACKUP_STORE = "drawingBAK";
 

src/view/ExcalidrawView.ts
@@ -166,7 +166,6 @@ import {
   excalidrawSword,
   ICONS,
   LogoWrapper,
-  Rank,
   saveIcon,
   SwordColors,
 } from "../constants/actionIcons";
@@ -3224,12 +3223,12 @@ export default class ExcalidrawView
       const notice = new Notice(t("MASK_FILE_NOTICE"), 5000);
       //add click and hold event listner to the notice
       let noticeTimeout: number;
-      this.registerDomEvent(notice.noticeEl, "pointerdown", () => {
+      this.registerDomEvent(notice.messageEl, "pointerdown", () => {
         noticeTimeout = window.setTimeout(() => {
           window.open(getYouTubeUrl("uHFd0XoHRxE"));
         }, 1000);
       });
-      this.registerDomEvent(notice.noticeEl, "pointerup", () => {
+      this.registerDomEvent(notice.messageEl, "pointerup", () => {
         window.clearTimeout(noticeTimeout);
       });
     }
@@ -5886,7 +5885,7 @@ export default class ExcalidrawView
 
         //if there are image elements
         //first delete corresponding "old" text elements
-        for (const [el, _] of imageElementsMap) {
+        for (const [ el ] of imageElementsMap) {
           const clone = cloneElement(el);
           clone.isDeleted = true;
           this.excalidrawData.deleteTextElement(clone.id);
@@ -6413,7 +6412,7 @@ export default class ExcalidrawView
         return;
       }
     }
-    const { folder, filepath: _ } = await getAttachmentsFolderAndFilePath(
+    const { folder } = await getAttachmentsFolderAndFilePath(
       this.app,
       this.file.path,
       "dummy",

src/constants/actionIcons.tsx
@@ -1,6 +1,7 @@
 import { getIcon, sanitizeHTMLToDom } from "obsidian";
 import { PenStyle } from "src/types/penTypes";
 import React from "react";
+import { mainDocument } from "./constants";
 
 const BLOCKED_SVG_TAGS = new Set([
   "script",
@@ -45,7 +46,7 @@ const isUnsafeAttribute = (name: string, value: string): boolean => {
 
 const sanitizeSvgTree = (svg: SVGSVGElement) => {
   const nodesToRemove: Element[] = [];
-  const walker = document.createTreeWalker(svg, NodeFilter.SHOW_ELEMENT);
+  const walker = mainDocument.createTreeWalker(svg, NodeFilter.SHOW_ELEMENT);
 
   let current = walker.currentNode as Element;
   while (current) {
@@ -82,7 +83,7 @@ const sanitizeSvgElement = (svg: SVGSVGElement): SVGSVGElement => {
 };
 
 const applyDefaultSvgTheme = (svg: SVGSVGElement) => {
-  const walker = document.createTreeWalker(svg, NodeFilter.SHOW_ELEMENT);
+  const walker = mainDocument.createTreeWalker(svg, NodeFilter.SHOW_ELEMENT);
   let current = walker.currentNode as Element;
   while (current) {
     current.removeAttribute("stroke");

src/lang/helpers.ts
@@ -21,7 +21,7 @@ import { errorHandler } from "src/utils/ErrorHandler";
 
 declare const PLUGIN_LANGUAGES: Record<string, string>;
 declare const PLUGIN_VERSION: string;
-declare var LZString: any;
+declare const LZString: any;
 
 let locale: Partial<typeof en> | null = null;
 

src/shared/CropImage.ts
@@ -3,7 +3,6 @@ import {
   FileId,
 } from "@zsviczian/excalidraw/types/element/src/types";
 import { BinaryFileData } from "@zsviczian/excalidraw/types/excalidraw/types";
-import { Mutable } from "@zsviczian/excalidraw/types/common/src/utility-types";
 import { Notice } from "obsidian";
 
 import { getEA } from "src/core";

src/shared/ExcalidrawAutomate.ts
@@ -202,7 +202,7 @@ import { AIRequest, ExcalidrawAISettings } from "src/types/AIUtilTypes";
 import { getAspectRatio } from "src/utils/YoutTubeUtils";
 import { getPDFCropRect } from "src/utils/PDFUtils";
 import { CaptureUpdateActionType } from "@zsviczian/excalidraw/types/element/src";
-import { URL_REGISTRY, URLs } from "src/constants/safeUrls";
+import { URL_REGISTRY } from "src/constants/safeUrls";
 
 type ExcalidrawAutomateHelpTarget = ((...args: unknown[]) => unknown) | string;
 

src/shared/ExcalidrawData.ts
@@ -860,7 +860,7 @@ export class ExcalidrawData {
 
     const timer = window.setTimeout(() => {
       const notice = new Notice(t("FONT_LOAD_SLOW"), 15000);
-      notice.noticeEl.oncontextmenu = () => {
+      notice.messageEl.oncontextmenu = () => {
         displayFontMessage(this.app);
       };
     }, 5000);
@@ -1193,7 +1193,13 @@ export class ExcalidrawData {
         te.y = y;
         te.width = width;
         te.height = height;
-      } catch (_e) {}
+      } catch (error) {
+        console.log(
+          "error updating text element dimensions, likely due to element being deleted meanwhile",
+          "updateSceneTextElements",
+          error,
+        );
+      }
     }
   }
 

src/utils/fileUtils.ts
@@ -389,7 +389,7 @@ export const readLocalFile = async (filePath: string): Promise<string> => {
     return null;
   }
   return new Promise((resolve, reject) => {
-    const adapter = app.vault.adapter as NodeFsDataAdapter;
+    const adapter = EXCALIDRAW_PLUGIN.app.vault.adapter as NodeFsDataAdapter;
     adapter.fs.readFile(filePath, "utf8", (err, data) => {
       if (err) {
         reject(err);
@@ -408,7 +408,7 @@ export const readLocalFileBinary = async (
   }
   return new Promise((resolve, reject) => {
     const path = decodeURI(filePath);
-    const adapter = app.vault.adapter as NodeFsDataAdapter;
+    const adapter = EXCALIDRAW_PLUGIN.app.vault.adapter as NodeFsDataAdapter;
     adapter.fs.readFile(path, (err, data) => {
       if (err) {
         reject(err);

commit 11f47ecd41b3e9ef45c4fa7a786a587230f81baa

Add style helpers step 1 fixed regression introduced in d3232f3071fd5e3af72810b7440597719857703c

src/core/main.ts
@@ -96,7 +96,7 @@ import {
   emulateCTRLClickForLinks,
   linkClickModifierType,
 } from "../utils/modifierkeyHelper";
-import { imageCache } from "../shared/ImageCache";
+import { getImageCache } from "../shared/ImageCache";
 import { StylesManager } from "./managers/StylesManager";
 import { CustomMutationObserver, log } from "../utils/debugHelper";
 import { ExcalidrawConfig } from "../shared/ExcalidrawConfig";
@@ -133,6 +133,7 @@ import {
   encryptPersistedAPIKeys,
 } from "src/utils/settingsKeyObfuscation";
 import { URLs } from "src/constants/safeUrls";
+import { hideElement, setButtonBgColor, showElement } from "src/utils/styleUtils";
 
 declare const PLUGIN_VERSION: string;
 declare const INITIAL_TIMESTAMP: number;
@@ -919,7 +920,7 @@ export default class ExcalidrawPlugin extends Plugin {
     this.logStartupEvent("Fonts initialized");
 
     try {
-      void imageCache.initializeDB(this);
+      void getImageCache().initializeDB(this);
     } catch (e) {
       new Notice("Error initializing image cache", 6000);
       console.error("Error initializing image cache", e);
@@ -1269,8 +1270,8 @@ export default class ExcalidrawPlugin extends Plugin {
         button2 = link.parentElement.createEl("button", null, (b) => {
           b.setText(t("UPDATE_SCRIPT"));
           b.addClass("mod-muted");
-          b.style.backgroundColor = "var(--interactive-success)";
-          b.style.display = "none";
+          setButtonBgColor(b, "success");
+          hideElement(b);
         });
       } catch (e) {
         errorlog({
@@ -1286,31 +1287,31 @@ export default class ExcalidrawPlugin extends Plugin {
           text: "CHECKING" | "INSTALL" | "UPTODATE" | "UPDATE" | "ERROR",
         ) => {
           if (button2) {
-            button2.style.display = "none";
+            hideElement(button2);
           }
           switch (text) {
             case "CHECKING":
               button.setText(t("CHECKING_SCRIPT"));
-              button.style.backgroundColor = "var(--interactive-normal)";
+              setButtonBgColor(button, "normal");
               break;
             case "INSTALL":
               button.setText(t("INSTALL_SCRIPT"));
-              button.style.backgroundColor = "var(--interactive-accent)";
+              setButtonBgColor(button, "accent");
               break;
             case "UPTODATE":
               button.setText(t("UPTODATE_SCRIPT"));
-              button.style.backgroundColor = "var(--interactive-normal)";
+              setButtonBgColor(button, "normal");
               break;
             case "UPDATE":
               button.setText(t("UPDATE_SCRIPT"));
-              button.style.backgroundColor = "var(--interactive-success)";
+              setButtonBgColor(button, "success");
               if (button2) {
-                button2.style.display = null;
+                showElement(button2);
               }
               break;
             case "ERROR":
               button.setText(t("UNABLETOCHECK_SCRIPT"));
-              button.style.backgroundColor = "var(--interactive-normal)";
+              setButtonBgColor(button, "normal");
               break;
           }
         };
@@ -1829,8 +1830,8 @@ export default class ExcalidrawPlugin extends Plugin {
       this.scriptEngine = null;
     }
 
-    if (imageCache) {
-      imageCache.destroy();
+    if (getImageCache()) {
+      getImageCache().destroy();
     }
 
     this.stylesManager.destroy();

src/core/managers/FileManager.ts
@@ -45,7 +45,7 @@ import {
   openLeaf,
 } from "src/utils/obsidianUtils";
 import { errorlog, getExportTheme } from "src/utils/utils";
-import { imageCache } from "src/shared/ImageCache";
+import { getImageCache } from "src/shared/ImageCache";
 import { PaneTarget } from "src/types/utilTypes";
 
 export class PluginFileManager {
@@ -694,10 +694,10 @@ export class PluginFileManager {
     //this will not work in a short period when Obsidian is starting up, however
     //because there is housekeeping in ImageCache at each startup to delete
     //BAK files, this is not a major issue.
-    if (!imageCache.isReady() || !path) {
+    if (!getImageCache().isReady() || !path) {
       return;
     }
-    await imageCache.removeBAKFromCache(path);
+    await getImageCache().removeBAKFromCache(path);
   }
 
   private async moveBAKFile(oldPath: string, newPath: string) {
@@ -708,12 +708,12 @@ export class PluginFileManager {
     //this will only effect a very few files, statistically unlikely to cause
     //much/any real user impact.
     //a proper queuing feels overkill for this.
-    if (!imageCache.isReady()) {
+    if (!getImageCache().isReady()) {
       return;
     }
-    const backup = await imageCache.getBAKFromCache(oldPath);
+    const backup = await getImageCache().getBAKFromCache(oldPath);
     if (backup) {
-      await imageCache.addBAKToCache(newPath, `${backup}`);
+      await getImageCache().addBAKToCache(newPath, `${backup}`);
       await this.removeBAKFromCache(oldPath);
     }
   }

src/core/managers/MarkdownPostProcessor.ts
@@ -31,7 +31,7 @@ import {
   getFileCSSClasses,
 } from "../../utils/obsidianUtils";
 import { linkClickModifierType } from "../../utils/modifierkeyHelper";
-import { ImageKey, imageCache } from "../../shared/ImageCache";
+import { ImageKey, getImageCache } from "../../shared/ImageCache";
 import { FILENAMEPARTS, PreviewImageType } from "../../types/utilTypes";
 import { CustomMutationObserver, DEBUGGING } from "../../utils/debugHelper";
 import { getExcalidrawFileForwardLinks } from "../../utils/excalidrawViewUtils";
@@ -120,7 +120,7 @@ const _getPNG = async ({
   };
 
   if (cacheReady) {
-    const src = await imageCache.getImageFromCache(cacheKey);
+    const src = await getImageCache().getImageFromCache(cacheKey);
     //In case of PNG I cannot change the viewBox to select the area of the element
     //being referenced. For PNG only the group reference works
     if (src && typeof src === "string") {
@@ -174,7 +174,7 @@ const _getPNG = async ({
     img.addEventListener("error", cleanup, { once: true });
   }
 
-  cacheReady && imageCache.addImageToCache(cacheKey, blobUrl, png);
+  cacheReady && getImageCache().addImageToCache(cacheKey, blobUrl, png);
   return img;
 };
 
@@ -241,7 +241,7 @@ const _getSVGIMG = async ({
   };
 
   if (cacheReady) {
-    const src = await imageCache.getImageFromCache(cacheKey);
+    const src = await getImageCache().getImageFromCache(cacheKey);
     if (src && typeof src === "string") {
       img.setAttribute("src", src);
       return img;
@@ -333,7 +333,7 @@ const _getSVGNative = async ({
   };
   let maybeSVG;
   if (cacheReady) {
-    maybeSVG = await imageCache.getImageFromCache(cacheKey);
+    maybeSVG = await getImageCache().getImageFromCache(cacheKey);
   }
 
   const svg =
@@ -381,7 +381,7 @@ const _getSVGNative = async ({
 
   //cache SVG should have the width and height parameters and not the embedded font
   if (!maybeSVG) {
-    cacheReady && imageCache.addImageToCache(cacheKey, "", svg);
+    cacheReady && getImageCache().addImageToCache(cacheKey, "", svg);
   }
 
   if (width && !isNaN(width)) {
@@ -446,7 +446,7 @@ const getIMG = async (
     theme ? theme === "dark" : undefined,
   );
 
-  const cacheReady = imageCache.isReady();
+  const cacheReady = getImageCache().isReady();
 
   await plugin.awaitInit();
   switch (plugin.settings.previewImageType) {
@@ -518,7 +518,7 @@ const addSVGToImgSrc = (
     img.addEventListener("error", cleanup, { once: true });
   }
 
-  cacheReady && imageCache.addImageToCache(cacheKey, blobUrl, blob);
+  cacheReady && getImageCache().addImageToCache(cacheKey, blobUrl, blob);
   return img;
 };
 

src/core/settings.ts
@@ -29,7 +29,7 @@ import {
 import { PENS } from "src/utils/pens";
 import { addYouTubeThumbnail, fragWithHTML } from "src/utils/utils";
 import { setElementIconAndText, setSanitizedHtml } from "src/utils/htmlUtils";
-import { imageCache } from "src/shared/ImageCache";
+import { getImageCache } from "src/shared/ImageCache";
 import { MultiOptionConfirmationPrompt } from "src/shared/Dialogs/Prompt";
 import { EmbeddableMDCustomProps } from "src/shared/Dialogs/EmbeddableSettings";
 import { EmbeddalbeMDFileCustomDataSettingsComponent } from "src/shared/Dialogs/EmbeddableMDFileCustomDataSettingsComponent";
@@ -70,6 +70,7 @@ import { getAIUsage, formatAIUsageLabel } from "src/utils/AIUtils";
 import { decryptProviderProfiles } from "src/utils/settingsKeyObfuscation";
 import { getGeminiSupportedSizes } from "src/utils/geminiImageModelUtils";
 import { URLs } from "src/constants/safeUrls";
+import { hideElement, showElement } from "src/utils/styleUtils";
 
 export interface ExcalidrawSettings {
   showTabTitlebarButtons: boolean;
@@ -1859,11 +1860,21 @@ export class ExcalidrawSettingTab extends PluginSettingTab {
         toggle
           .setValue(this.plugin.settings.aiEnabled ?? true)
           .onChange(async (value) => {
-            aiEl.style.display = value ? "block" : "none";
+            if (value) {
+              showElement(aiEl);
+            } else {
+              hideElement(aiEl);
+            }
             this.plugin.settings.aiEnabled = value;
             this.applySettingsUpdate();
           }),
       );
+
+    detailsEl = detailsEl.createDiv();
+    const aiEl = detailsEl;
+    if (!(this.plugin.settings.aiEnabled ?? true)) {
+      hideElement(detailsEl);
+    }
 
     new Setting(detailsEl)
       .setName(t("AI_USAGE_SETTINGS_BUTTON_NAME"))
@@ -1890,12 +1901,6 @@ export class ExcalidrawSettingTab extends PluginSettingTab {
             this.applySettingsUpdate();
           }),
       );
-
-    detailsEl = detailsEl.createDiv();
-    const aiEl = detailsEl;
-    if (!(this.plugin.settings.aiEnabled ?? true)) {
-      detailsEl.style.display = "none";
-    }
 
     let selectedProviderProfile =
       Object.keys(this.plugin.settings.aiProviderProfiles ?? {})[0] || "OpenAI";
@@ -2919,7 +2924,11 @@ export class ExcalidrawSettingTab extends PluginSettingTab {
           .setValue(this.plugin.settings.gridSettings.DYNAMIC_COLOR)
           .onChange(async (value) => {
             this.plugin.settings.gridSettings.DYNAMIC_COLOR = value;
-            gridColorSection.style.display = value ? "none" : "block";
+            if (value) {
+              hideElement(gridColorSection);
+            } else {
+              showElement(gridColorSection);
+            }
             this.applySettingsUpdate();
             updateGridColor();
           }),
@@ -2927,10 +2936,11 @@ export class ExcalidrawSettingTab extends PluginSettingTab {
 
     // Create a div to contain color and opacity settings
     const gridColorSection = detailsEl.createDiv();
-    gridColorSection.style.display = this.plugin.settings.gridSettings
-      .DYNAMIC_COLOR
-      ? "none"
-      : "block";
+    if (this.plugin.settings.gridSettings.DYNAMIC_COLOR) {
+      hideElement(gridColorSection);
+    } else {
+      showElement(gridColorSection);
+    }
 
     // Grid color picker
     new Setting(gridColorSection)
@@ -3536,7 +3546,7 @@ export class ExcalidrawSettingTab extends PluginSettingTab {
       .setName(t("EMBED_IMAGE_CACHE_CLEAR"))
       .addButton((button) =>
         button.setButtonText(t("EMBED_IMAGE_CACHE_CLEAR")).onClick(() => {
-          void imageCache.clearImageCache();
+          void getImageCache().clearImageCache();
         }),
       );
     new Setting(detailsEl)
@@ -3549,7 +3559,7 @@ export class ExcalidrawSettingTab extends PluginSettingTab {
           );
           void confirmationPrompt.waitForClose.then((confirmed) => {
             if (confirmed) {
-              void imageCache.clearBackupCache();
+              void getImageCache().clearBackupCache();
             }
           });
         }),

src/shared/EmbeddedFileLoader.ts
@@ -58,7 +58,7 @@ import {
   shouldRenderMermaid,
 } from "../utils/mermaidUtils";
 import { mermaidToExcalidraw } from "src/constants/constants";
-import { ImageKey, imageCache } from "./ImageCache";
+import { ImageKey, getImageCache } from "./ImageCache";
 import { FILENAMEPARTS, PreviewImageType } from "../types/utilTypes";
 import {
   ColorMap,
@@ -495,7 +495,7 @@ export class EmbeddedFilesLoader {
       !hasColorMap &&
       this.plugin.settings.allowImageCacheInScene &&
       file &&
-      imageCache.isReady();
+      getImageCache().isReady();
     const hasFilenameParts = Boolean(
       inFile instanceof EmbeddedFile && inFile.filenameparts,
     );
@@ -530,7 +530,7 @@ export class EmbeddedFilesLoader {
     };
 
     const maybeSVG = shouldUseCache
-      ? await imageCache.getImageFromCache(cacheKey, {
+      ? await getImageCache().getImageFromCache(cacheKey, {
           skipDependencyCheck: cacheValidation === "stale-first",
         })
       : undefined;
@@ -618,7 +618,7 @@ export class EmbeddedFilesLoader {
     if (shouldUseCache && !maybeSVG) {
       //cache SVG should have the width and height parameters and not the embedded font
       //see svgWithFont below
-      imageCache.addImageToCache(cacheKey, "", svg);
+      getImageCache().addImageToCache(cacheKey, "", svg);
     }
 
     if (!svg.hasAttribute("width") && svg.hasAttribute("viewBox")) {
@@ -1122,7 +1122,7 @@ export class EmbeddedFilesLoader {
       const pageNum = isNaN(linkParts.page) ? 1 : (linkParts.page ?? 1);
       const requestedScale = this.plugin.settings.pdfScale;
       const shouldUseCache =
-        imageCache.isReady() &&
+        getImageCache().isReady() &&
         (!options || this.plugin.settings.allowImageCacheInScene);
       const cacheKey: ImageKey = {
         filepath: file.path,
@@ -1145,7 +1145,7 @@ export class EmbeddedFilesLoader {
         inlineFonts: false,
       };
       const cachedData = shouldUseCache
-        ? await imageCache.getImageCacheData(cacheKey, {
+        ? await getImageCache().getImageCacheData(cacheKey, {
             skipDependencyCheck: options?.cacheValidation === "stale-first",
             minRenderScale:
               options?.cacheValidation === "stale-first"
@@ -1335,7 +1335,7 @@ export class EmbeddedFilesLoader {
         });
         const base64 = await blobToBase64(blob);
         shouldUseCache &&
-          imageCache.addImageToCache(cacheKey, "", blob, {
+          getImageCache().addImageToCache(cacheKey, "", blob, {
             renderScale: requestedScale,
             size: { width, height },
             pdfPageViewProps: viewProps,

src/shared/ImageCache.ts
@@ -5,9 +5,6 @@ import { convertSVGStringToElement } from "../utils/utils";
 import { FILENAMEPARTS, PreviewImageType } from "../types/utilTypes";
 import { hasExcalidrawEmbeddedImagesTreeChanged } from "../utils/fileUtils";
 import { EXCALIDRAW_PLUGIN } from "src/constants/constants";
-const DB_NAME = `Excalidraw ${EXCALIDRAW_PLUGIN.app.appId}`;
-const CACHE_STORE = "imageCache";
-const BACKUP_STORE = "drawingBAK";
 
 type FileCacheData = {
   mtime: number;
@@ -607,4 +604,14 @@ class ImageCache {
   }
 }
 
-export const imageCache = new ImageCache(DB_NAME, CACHE_STORE, BACKUP_STORE);
+let imageCache: ImageCache = null;
+
+export const getImageCache = (): ImageCache => {
+  if (!imageCache) {
+    const DB_NAME = `Excalidraw ${EXCALIDRAW_PLUGIN.app.appId}`;
+    const CACHE_STORE = "imageCache";
+    const BACKUP_STORE = "drawingBAK";
+    imageCache = new ImageCache(DB_NAME, CACHE_STORE, BACKUP_STORE);
+  }
+  return imageCache;
+};

src/utils/styleUtils.ts
@@ -1,0 +1,14 @@
+export const setButtonBgColor = (button: HTMLButtonElement, state: "normal" | "accent" | "success") => {
+    button.removeClass("excalidraw-button-bgcolor-normal");
+    button.removeClass("excalidraw-button-bgcolor-accent");
+    button.removeClass("excalidraw-button-bgcolor-success");
+    button.addClass(`excalidraw-button-bgcolor-${state}`);
+};
+
+export const hideElement = (el: HTMLElement) => {
+    el.addClass("excalidraw-display-none");
+};
+
+export const showElement = (el: HTMLElement) => {
+    el.removeClass("excalidraw-display-none");
+};

src/view/ExcalidrawView.ts
@@ -202,7 +202,7 @@ import {
   tmpBruteForceCleanup,
   toggleImageAnchoring,
 } from "../utils/excalidrawViewUtils";
-import { imageCache } from "../shared/ImageCache";
+import { getImageCache } from "../shared/ImageCache";
 import { CanvasNodeFactory } from "./managers/CanvasNodeFactory";
 import { EmbeddableMenu } from "./components/menu/EmbeddableActionsMenu";
 import { useDefaultExcalidrawFrame } from "../utils/customEmbeddableUtils";
@@ -1167,7 +1167,7 @@ export default class ExcalidrawView
             }
             await plugin.app.vault.modify(file, d);
             //this is a shady edge case, don't scrifice the BAK file in case the drawing is empty
-            //await imageCache.addBAKToCache(file.path,d);
+            //await getImageCache().addBAKToCache(file.path,d);
           }, 200);
           this.semaphores.saving = false;
           return;
@@ -1180,7 +1180,7 @@ export default class ExcalidrawView
         const data = this.lastSavedData;
         //if the scene is empty, do not save to BAK (this could be due to a crash when the BAK should not be updated)
         if (scene && scene.elements && scene.elements.length > 0) {
-          window.setTimeout(() => imageCache.addBAKToCache(path, data), 50);
+          window.setTimeout(() => getImageCache().addBAKToCache(path, data), 50);
         }
         triggerReload =
           this.lastSaveTimestamp === this.file.stat.mtime &&
@@ -3298,9 +3298,9 @@ export default class ExcalidrawView
             let confirmation: boolean | null = true;
             let counter = 0;
             const timestamp = Date.now();
-            while (!imageCache.isReady() && confirmation) {
+            while (!getImageCache().isReady() && confirmation) {
               const message = `You've been now waiting for <b>${Math.round((Date.now() - timestamp) / 1000)}</b> seconds. `;
-              imageCache.initializationNotice = true;
+              getImageCache().initializationNotice = true;
               const confirmationPrompt = new MultiOptionConfirmationPrompt(
                 plugin,
                 `${
@@ -3325,7 +3325,7 @@ export default class ExcalidrawView
               counter++;
             }
 
-            const drawingBAK = await imageCache.getBAKFromCache(file.path);
+            const drawingBAK = await getImageCache().getBAKFromCache(file.path);
             if (!drawingBAK) {
               new Notice(
                 `Error loading drawing:\n${e.message}${
@@ -3356,12 +3356,12 @@ export default class ExcalidrawView
       }
 
       if (
-        imageCache.isReady() &&
+        getImageCache().isReady() &&
         this.excalidrawData.scene &&
         this.excalidrawData.scene.elements &&
         this.excalidrawData.scene.elements.length === 0
       ) {
-        const backup = await imageCache.getBAKFromCache(this.file.path);
+        const backup = await getImageCache().getBAKFromCache(this.file.path);
         if (backup && backup.length > data.length) {
           window.setTimeout(async () => {
             const confirmationPrompt = new MultiOptionConfirmationPrompt(
@@ -3386,10 +3386,10 @@ export default class ExcalidrawView
                 path,
                 backup,
               );
-              await imageCache.removeBAKFromCache(this.file.path);
+              await getImageCache().removeBAKFromCache(this.file.path);
               this.plugin.openDrawing(backupFile, "new-tab");
             } else if (result === 2) {
-              await imageCache.removeBAKFromCache(this.file.path);
+              await getImageCache().removeBAKFromCache(this.file.path);
             }
           });
         }

styles.css
@@ -994,3 +994,9 @@ End Obsidian Sheets Plus support
 .excalidraw:not(.excalidraw--tray) .HintViewer {
   position: absolute;
 }
+
+.excalidraw-button-bgcolor {
+  &-normal { background-color: var(--interactive-normal); }
+  &-accent { background-color: var(--interactive-accent); }
+  &-success { background-color: var(--interactive-success); }
+}